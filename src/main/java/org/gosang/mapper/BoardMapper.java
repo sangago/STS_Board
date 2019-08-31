7e9ad015da3830b2589aca5566dea9bd6d0e785c
sangago <sangago.dev@gmail.com>
2019-08-31 11:00:15 +0900

    test: Jenkins PMD error
    - unnecessary modifier 'public' on method ........

--- src/main/java/org/gosang/mapper/BoardMapper.java
+++ src/main/java/org/gosang/mapper/BoardMapper.java
@@ -10,22 +10,22 @@ public interface BoardMapper {
 
 //	@Select("select * from tbl_board where bno > 0 ") BoardMapper.xml에 있음 
 	
-	public List<BoardVO> getList();
+	List<BoardVO> getList();
 	
-	public List<BoardVO> getListWithPaging(Criteria cri);
+	List<BoardVO> getListWithPaging(Criteria cri);
 	
-	public void register(BoardVO board);
+	void register(BoardVO board);
 	
-	public BoardVO read(Integer bno);
+	BoardVO read(Integer bno);
 	
-	public int delete(Integer bno);
+	int delete(Integer bno);
 	
-	public int update(BoardVO board);
+	int update(BoardVO board);
 	
-	public int getTotalCount(Criteria cri);	// 전체 데이터 개수 처리 
+	int getTotalCount(Criteria cri);	// 전체 데이터 개수 처리 
 	
-	public void updateReplyCnt(@Param("bno") int bno, @Param("amount") int amount);
+	void updateReplyCnt(@Param("bno") int bno, @Param("amount") int amount);
 
-	public void insertSelectKey(BoardVO board);
+	void insertSelectKey(BoardVO board);
 
 }
