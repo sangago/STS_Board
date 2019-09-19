33832ae7115b6ec3d88e679fbc6e83ae95319525
sangago <sangago.dev@gmail.com>
2019-09-19 20:58:44 +0900

    feat: 회원가입시 비밀번호 암호화

--- src/main/java/org/gosang/service/SignupServiceImpl.java
+++ src/main/java/org/gosang/service/SignupServiceImpl.java
@@ -3,6 +3,7 @@ package org.gosang.service;
 import org.gosang.domain.SignupVO;
 import org.gosang.mapper.SignupMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.Setter;
@@ -12,11 +13,19 @@ import lombok.extern.log4j.Log4j;
 @Service
 public class SignupServiceImpl implements SignupService {
 	
+	@Setter(onMethod_ = @Autowired)	// 암호화된 문자
+	private PasswordEncoder pwencoder;
+	
 	@Setter(onMethod_ = @Autowired)
 	private SignupMapper mapper;
 	
 	@Override
 	public void register(SignupVO signup) {
+		
+		String encUserpw = pwencoder.encode(signup.getUserpw()); //Userpw를 암호화 하여 문자열로 만든다
+		log.info("암호화된 비밀번호: " + encUserpw);
+		
+		signup.setUserpw(encUserpw);		// 암호화된 encUserpw를 userpw에 넣는다
 		
 		log.info("register.........." + signup);
 		
